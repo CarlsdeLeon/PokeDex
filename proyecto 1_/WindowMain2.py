e149d5a4c9c4d851929a0da8e033c82be8e09d75
CarlsdeLeon <124699853+CarlsdeLeon@users.noreply.github.com>
2024-03-19 09:23:21 -0600

    Arreglos en mostrar los datos

--- proyecto 1_/WindowMain2.py
+++ proyecto 1_/WindowMain2.py
@@ -39,5 +39,5 @@ class Ui_MainWindow2(object):
         self.textEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:22pt; color:#aaaa7f;\"><p>" + str(data) + "</p></span></p></body></html>"))
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:10.8pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:22pt; color:#aaaa7f;\">" + str(data) + "</span></p></body></html>"))
